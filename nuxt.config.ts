c1c8ea73e9d37215e3cd6235f4299ac6d5234232
Syafiq-Rapngai-Palo <srapngai@palo-it.com>
2026-04-14 16:26:57 +0800

    fix: something again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
   app: {
-    baseURL: "/nuxtjs-github-actions/"
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },

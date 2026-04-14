7b7eb9f025581c0513609a334f3b99c02814292c
Syafiq-Rapngai-Palo <srapngai@palo-it.com>
2026-04-14 15:37:55 +0800

    feat: first commit

---
+++ app/app.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const textRef = ref('');
+const confirmedName = ref('');
+
+function onConfirmName() {
+  confirmedName.value = textRef.value;
+}
+</script>
+
+<template>
+  <div class="flex gap-2 m-4 items-center">
+    <label for="text">Insert name here</label>
+    <input
+      class="border-green-300 focus:border-green-700 border outline-0 p-2"
+      v-model="textRef"
+      type="text"
+      name="text"
+    />
+    <button
+      class="bg-green-700 hover:bg-green-600 transition hover:cursor-pointer border-green-900 text-white p-2 rounded"
+      v-on:click="onConfirmName"
+    >
+      Confirm name
+    </button>
+  </div>
+
+  <div class="m-4" v-if="confirmedName">
+    <h1 class="text-4xl">Hello {{ confirmedName }}!</h1>
+  </div>
+</template>

aa1e6bbacd980d931ac179088289e77b01dae003
Syafiq-Rapngai-Palo <srapngai@palo-it.com>
2026-04-14 16:04:31 +0800

    fix: configured nuxt app base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || "/"
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
